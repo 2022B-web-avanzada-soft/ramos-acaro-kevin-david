f28e0afd642ab6e78dd7a2d2d3704b41bd9c8b97
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2022-11-14 17:56:31 -0500

    Hasta variables primitivas

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,27 @@
+//01-javascript
+//      01-variables.js
+
+//mutables (re asignadas =) e inmutables
+
+var numeroUno = 1
+let numeroDos = 2
+
+
+//Inmutables
+const configuracionDeArchivos = 'PDF'
+//vamos a preferir  CONST > LET > NUNCA VAR!
+
+//Tipo de Variables (primitivas)
+const numero = 1  //number
+const sueldo = 1.2 //number
+const texto = 'Kevin' //string
+const boolean = true //boolean
+const hijos = null // object
+const zapatos = 1 // undefined
+
+console.log(typeof numero)
+console.log(typeof sueldo)
+console.log(typeof texto)
+console.log(typeof boolean)
+console.log(typeof hijos)
+console.log(typeof zapatos)
