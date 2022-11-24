c7e86e17f27948013f8d25c4ec1021b3fbd3a8d0
Kevin Ramos <kevin.ramos01@epn.edu.ec>
2022-11-24 17:53:16 -0500

    Hasta callback

---
+++ 01-javascript/06-callbacks.js
@@ -0,0 +1,59 @@
+const fs = require('fs'); // file system
+                            //Importar modulo fs
+
+//06-ejemplo.txt -> Hola
+console.log("PRIMERO");
+fs.readFile(
+    "./06-ejemplo.txt", //Nombre o path del archivo
+    "utf-8", //codificacion
+    (errorLecturaPrimerArchivo, contenidoPrimerArchivo) => { //callback
+        if(errorLecturaPrimerArchivo){
+            console.log("ERROR LEYENDO ARCHIVO", errorLecturaPrimerArchivo);
+        }else{
+            console.log("Contenido:", contenidoPrimerArchivo);
+        }
+        console.log("SEGUNDO");
+    }
+);
+
+console.log("TERCERO");
+
+
+fs.readFile(
+    "./06-ejemplo.txt", //Nombre o path del archivo
+    "utf-8", //codificacion
+    (errorLecturaPrimerArchivo, contenidoPrimerArchivo) => { //callback
+        if(errorLecturaPrimerArchivo){
+            console.log("ERROR LEYENDO ARCHIVO", errorLecturaPrimerArchivo);
+        }else{
+            console.log("Contenido:", contenidoPrimerArchivo);
+            fs.readFile(
+                "./01-variables.js", //Nombre o path del archivo
+                "utf-8", //codificacion
+                (errorLecturaPrimerArchivo, contenidoSegundoArchivo) => { //callback
+                    if(errorLecturaPrimerArchivo){
+                        console.log("ERROR LEYENDO ARCHIVO", errorLecturaPrimerArchivo);
+                    }else{
+                        console.log("Contenido:", contenidoSegundoArchivo);
+                        fs.writeFile(
+                            "./06-nuevo-archivo.txt",
+                            contenidoPrimerArchivo + contenidoSegundoArchivo,
+                            (errorEscritura) => {
+                                if(errorEscritura){
+                                    console.log("ERROR ESCRIBIR ARCHIVO", errorEscritura);
+
+                                }
+                            }
+                        );
+                    }
+
+                }
+            );
+        }
+
+    }
+);
+async
+
+
+
